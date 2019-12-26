4aab06ea4dfeade8f122b5b8d15020c5b90a048c
Nishkal <nishkal8463@gmail.com>
2019-12-26 19:13:35 +0530

    Adding files

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (lat,long,callback) => {
+    const url = 'https://api.darksky.net/forecast/8baf51dcc78cb0217490e928b1d1b66e/'+lat+','+long
+    request({url : url,json : true},(error,response) => {
+        if(error){
+            callback('Unable to connect to weather services',undefined)
+        }else if(response.body.error){
+            callback('Unable to find location',undefined)      
+        }else{
+            callback(undefined,{
+                forecast: response.body.currently.summary+'. It is currently '+response.body.currently.temperature+' degrees out. There is '+response.body.currently.precipProbability+'% chance of rain'
+            })
+        }
+    })
+}
+
+module.exports= forecast
